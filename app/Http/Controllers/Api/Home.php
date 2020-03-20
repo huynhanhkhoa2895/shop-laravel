57863caa0c4d63e6c889341466cc53358da488bb
huynhanhkhoa2895 <huynhanhkhoa2895@gmail.com>
2020-03-20 17:45:56 +0700

    fix

--- app/Http/Controllers/Api/Home.php
+++ app/Http/Controllers/Api/Home.php
@@ -1,33 +1,28 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Factory\Cart;
 use App\Model\M_Model;
 class Home extends Controller
 {
     public $_model;
     function __construct(){
-        $this->_model = new M_Model;
+        $this->_model = new M_Model();
     }
     public function test(){
         $cart = new Cart(1,2);
-        echo "<pre>";
-        print_r($cart->getCart(1));
-        echo "</pre>";
     }
     public function getListMenu(){
         return response()->json($this->_model->getListHeader());
     }
     public function getCategory(Request $rq){
         $route = $this->_model->getIdByRoute($rq->route);
-        if($route->table == "category" || $route->table == "brand"){
-            $product = $this->_model->getListById($route->table,$route->origin);
-        }else{
-            $product = $this->_model->getListById($route->table,$route->origin);
-        }
-        return response()->json($product);
+        $info = $this->_model->getInfo($route->table,$route->origin);
+        return response()->json(["info"=>$info,"table"=>$route->table]);
     }
     public function getListProduct(Request $rq){
         $arrProduct = [];
@@ -50,3 +45,4 @@ class Home extends Controller
         return response()->json(['product'=>$product,'image'=>$image]);
     }
 }
+
